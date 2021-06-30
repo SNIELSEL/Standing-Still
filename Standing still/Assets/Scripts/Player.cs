cb6286335dd41e287338b54d0f70878be9e1c959
SNIELSEL <niels@haverkotte.nl>
2021-07-01 00:20:15 +0200

    added movement and imported textmeshpro

---
+++ Standing still/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float PlayerSpeed = 500;
+    public float directionalspeed = 20;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(Mathf.Clamp(gameObject.transform.position.x + moveHorizontal, -2.5f, 2.5f), gameObject.transform.position.y,gameObject.transform.position.z), directionalspeed * Time.deltaTime);
+#endif
+        GetComponent<Rigidbody>().velocity = Vector3.forward * PlayerSpeed * Time.deltaTime;
+        //MOBILE CONTROLLS
+        Vector2 touch = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10f));
+        if (Input.touchCount > 0)
+        {
+            transform.position = new Vector3(touch.x, transform.position.y, transform.position.z);
+        }
+    }
+}
