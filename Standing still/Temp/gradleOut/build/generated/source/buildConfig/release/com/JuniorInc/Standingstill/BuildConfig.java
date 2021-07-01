4cbda3374e79fb6a9073c157a934b955970fcdac
SNIELSEL <niels@haverkotte.nl>
2021-07-01 04:28:07 +0200

    made another test build

---
+++ Standing still/Temp/gradleOut/build/generated/source/buildConfig/release/com/JuniorInc/Standingstill/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.JuniorInc.Standingstill;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.JuniorInc.Standingstill";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
